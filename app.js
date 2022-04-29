51b65a27195072f790f2844b16b0b360078a800f
carlosalbertoGT <carlosagt7@gmail.com>
2022-04-29 01:03:50 -0500

    Paso 5: Agregando route /launchx

--- app.js
+++ app.js
@@ -4,4 +4,9 @@ const port = 3000;
 
 app.get( '/', (req, res) => res.send('Hello World!'));
 
+
+app.get( '/launchx', (req, res) => {
+    res.send("Bienvenidos a launchX!");
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
